055a6b6ea5d2b65e0b7dc8fe6a13dbee8dde941a
Asjad Nasim <asjadnasim@gmail.com>
2024-06-26 13:04:44 +0530

    Custom Filters

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import CarCard from '@/components/CarCard';
 import CustomFilter from '@/components/CustomFilter';
 import Hero from '@/components/Hero';
 import SearchBar from '@/components/SearchBar';
+import { fuels, yearsOfProduction } from '@/constants';
 import { FilterProps } from '@/types';
 import { fetchCars } from '@/utils';
 
@@ -36,8 +37,8 @@ export default async function Home({ searchParams }: HomeProps) {
 					<SearchBar />
 
 					<div className='home__filter-container'></div>
-					{/* <CustomFilter title='fuel' /> */}
-					{/* <CustomFilter title='year' /> */}
+					<CustomFilter title='fuel' options={fuels} />
+					<CustomFilter title='year' options={yearsOfProduction} />
 				</div>
 
 				{!isDataEmpty ? (
